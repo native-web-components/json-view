f149cacf65dc5bf951669db344c6a9511cdb8a5f
sanmu <578595193@qq.com>
2024-06-08 14:42:24 +0800

    FEAT: support click copy

--- src/index.ts
+++ src/index.ts
@@ -1,108 +1,3 @@
-// import styles from "./styles.scss?inline";
-// class WebComponent extends HTMLElement {
-//   [key: string]: any;
-//   data: string = "";
-//   space: number = 2;
-//   color: boolean = true;
-//   // 监听的属性列表
-//   static get observedAttributes() {
-//     return ["data", "space", "color"];
-//   }
-//   constructor() {
-//     super();
-//     this.attachShadow({ mode: "open" });
-//     // const div = document.createElement("div");
-//     // div.innerHTML = "Hello World";
-//     // this.shadowRoot!.appendChild(div);
-//   }
-
-//   connectedCallback() {
-//     // console.log("connectedCallback when Custom element added to page.");
-//     this.render();
-//   }
-
-//   // disconnectedCallback() {
-//   //   console.log("disconnectedCallback when Custom element removed from page.");
-//   // }
-
-//   attributeChangedCallback(name: string, _oldValue: string, newValue: string) {
-//     switch (name) {
-//       case "data":
-//         this.data = newValue;
-//         break;
-//       case "space":
-//         this.space = Number(newValue);
-//         break;
-//       case "color":
-//         this.color = newValue === "true";
-//         break;
-//     }
-//     this.render();
-//   }
-
-//   // adoptedCallback() {
-//   //   console.log(
-//   //     "adoptedCallback when Custom element adopted into new document."
-//   //   );
-//   // }
-
-//   render() {
-//     if (this.data) {
-//       const data = JSON.stringify(
-//         JSON.parse(this.data),
-//         function replacer(_key, value) {
-//           return value;
-//         },
-//         this.space
-//       );
-//       this.shadowRoot!.innerHTML = `<pre>${
-//         this.color ? this.syntaxHighlight(data) : data
-//       }</pre>`;
-//     } else {
-//       this.shadowRoot!.innerHTML = `<pre>${this.data}</pre>`;
-//     }
-//     this.injectStyles();
-//   }
-
-//   injectStyles() {
-//     const style = document.createElement("style");
-//     style.textContent = `${styles}`;
-//     this.shadowRoot!.appendChild(style);
-//   }
-
-//   syntaxHighlight(jsonString: string) {
-//     let json = jsonString
-//       .replace(/&/g, "&")
-//       .replace(/</g, "<")
-//       .replace(/>/g, ">");
-//     return json.replace(
-//       /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\\-]?\d+)?)/g,
-//       function (match, ...args) {
-//         console.log(123, match, args);
-//         var cls = "pre-number";
-//         if (/^"/.test(match)) {
-//           if (/:$/.test(match)) {
-//             cls = "pre-key";
-//           } else {
-//             cls = "pre-string";
-//           }
-//         } else if (/true|false/.test(match)) {
-//           cls = "pre-boolean";
-//         } else if (/null/.test(match)) {
-//           cls = "pre-null";
-//         }
-//         return '<span class="' + cls + '">' + match + "</span>";
-//       }
-//     );
-//   }
-// }
-
-// const define = (name: string, options?: ElementDefinitionOptions) => {
-//   customElements.define(name, WebComponent, options);
-// }
-
-// export { define };
-
 import BaseComponent from "./BaseComponent";
 class WebComponent extends BaseComponent {
   data: string = "";
@@ -128,11 +23,26 @@ class WebComponent extends BaseComponent {
     this.shadowRoot!.appendChild(container);
     if (this.data) {
       const json = JSON.parse(this.data);
-      createJsonTree(json, container);
-    } else {
-      container.innerHTML = `<pre>${this.data}</pre>`;
+      this.addCopyButton(container);
+      if (typeof json === "object" && json !== null) {
+        const jsonObject = createElement("div", "json-object");
+        container.appendChild(jsonObject);
+        return createJsonTree(json, jsonObject);
+      }
     }
-    this.injectStyles();
+    container.innerHTML = this.data;
+  }
+
+  addCopyButton(container: HTMLElement) {
+    const button = createElement("button", "copy-button", "Copy");
+    container.appendChild(button);
+    button.addEventListener("click", () => {
+      const json = JSON.parse(this.data);
+      const jsonString = JSON.stringify(json, null, 2);
+      navigator.clipboard.writeText(jsonString).then(() => {
+        alert("JSON copied to clipboard");
+      });
+    });
   }
 }
 
@@ -144,22 +54,44 @@ function createElement(type: string, className?: string, innerText?: string) {
 }
 
 function createJsonTree(json: any, container: HTMLElement) {
+  if (Array.isArray(json)) {
+    container.appendChild(createElement("span", "bracket", "["));
+  } else {
+    container.appendChild(createElement("span", "bracket", "{"));
+  }
+  container.appendChild(createElement("br"));
+
   const keys = Object.keys(json);
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
     const isLastKey = i === keys.length - 1;
     if (json.hasOwnProperty(key)) {
-      const keyElement = createElement("span", "json-key", key);
-      container.appendChild(keyElement);
+      // 数组下标不追加key
+      if (!Array.isArray(json)) {
+        const keyElement = createElement("span", "json-key", key);
+        container.appendChild(keyElement);
+        let colonElement = createElement("span", "colon", ":");
+        container.appendChild(colonElement);
+      }
 
       if (typeof json[key] === "object" && json[key] !== null) {
-        const toggleElement = createElement("span", "json-toggle", " [+]");
+        const toggleElement = createElement(
+          "span",
+          "json-toggle",
+          `[+]${Array.isArray(json[key]) ? "Array" : "Object"}(${
+            Object.keys(json[key]).length
+          })`
+        );
         toggleElement.onclick = function (event: any) {
           const sibling = event.target!.nextElementSibling;
           sibling.classList.toggle("hidden");
           event.target.innerText = sibling.classList.contains("hidden")
-            ? " [+]"
-            : " [-]";
+            ? `[+]${Array.isArray(json[key]) ? "Array" : "Object"}(${
+                Object.keys(json[key]).length
+              })`
+            : `[-]${Array.isArray(json[key]) ? "Array" : "Object"}(${
+                Object.keys(json[key]).length
+              })`;
         };
         container.appendChild(toggleElement);
 
@@ -171,7 +103,7 @@ function createJsonTree(json: any, container: HTMLElement) {
         const valueElement = createElement(
           "span",
           "json-value",
-          `: ${JSON.stringify(value)}`
+          `${JSON.stringify(value)}`
         );
 
         if (typeof value === "string") {
@@ -192,6 +124,12 @@ function createJsonTree(json: any, container: HTMLElement) {
 
       container.appendChild(createElement("br"));
     }
+  }
+
+  if (Array.isArray(json)) {
+    container.appendChild(createElement("span", "bracket", "]"));
+  } else {
+    container.appendChild(createElement("span", "bracket", "}"));
   }
 }
 
